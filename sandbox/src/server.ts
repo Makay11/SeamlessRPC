29f293977dcf54a0d9ec8cae839f22b13188d631
Diogo Pais <dpais11@gmail.com>
2024-12-20 15:30:04 +0100

    Add logger

--- sandbox/src/server.ts
+++ sandbox/src/server.ts
@@ -2,10 +2,13 @@ import { serve } from "@hono/node-server"
 import { createRpc } from "@makay/rpc/hono"
 import { Hono } from "hono"
 import { cors } from "hono/cors"
+import { logger } from "hono/logger"
 
 const app = new Hono()
 
 app.use(
+	logger(),
+
 	cors({
 		origin: "http://localhost:5173",
 		credentials: true,
