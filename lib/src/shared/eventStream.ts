fc8658ccae211cc1ed1b2c05e34fbd5f5a71ae62
Diogo Pais <dpais11@gmail.com>
2024-12-30 17:25:22 +0100

    Improve event stream

--- lib/src/shared/eventStream.ts
+++ lib/src/shared/eventStream.ts
@@ -1,8 +1,12 @@
 import type { JsonValue } from "type-fest"
 
-export type Setup<T> = (emit: Emit<T>) => Cleanup
+export type Setup<T> = (controller: Controller<T>) => Cleanup
 
-export type Emit<T> = (value: T) => void
+export type Controller<T> = {
+	enqueue: (value: T) => void
+	close: () => void
+	error: (error?: unknown) => void
+}
 
 export type Cleanup = () => void
 
@@ -11,8 +15,18 @@ export function eventStream<T extends JsonValue>(setup: Setup<T>) {
 
 	return new ReadableStream<T>({
 		start(controller) {
-			cleanup = setup((value) => {
-				controller.enqueue(value)
+			cleanup = setup({
+				enqueue(value) {
+					controller.enqueue(value)
+				},
+				close() {
+					controller.close()
+					cleanup()
+				},
+				error(error) {
+					controller.error(error)
+					cleanup()
+				},
 			})
 		},
 		cancel() {
@@ -20,3 +34,45 @@ export function eventStream<T extends JsonValue>(setup: Setup<T>) {
 		},
 	})
 }
+
+// const stream = eventStream<string>((ctrl) => {
+// 	const interval = setInterval(() => {
+// 		console.log("emit")
+// 		ctrl.enqueue(new Date().toISOString())
+// 		// ctrl.close()
+// 		// ctrl.error("fake_error")
+// 	}, 1000)
+
+// 	return () => {
+// 		console.log("clean up")
+// 		clearInterval(interval)
+// 	}
+// })
+
+// const reader = stream.getReader()
+
+// try {
+// 	for (let i = 0; ; i++) {
+// 		const { done, value } = await reader.read()
+
+// 		if (done) {
+// 			console.log("close")
+// 			break
+// 		}
+
+// 		console.log(value)
+
+// 		if (i === 2) {
+// 			console.log("cancel early")
+// 			await reader.cancel()
+// 			break
+// 		}
+// 	}
+// } catch (error) {
+// 	console.log("catch")
+// 	console.error(error)
+// } finally {
+// 	console.log("finally")
+// 	reader.releaseLock()
+// 	console.log("all done")
+// }
