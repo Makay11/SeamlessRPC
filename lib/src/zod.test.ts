2588e7b9e71b1267d6d52a3c15caff94618889f9
Diogo Pais <dpais11@gmail.com>
2025-01-06 13:43:23 +0100

    Update test

--- lib/src/zod.test.ts
+++ lib/src/zod.test.ts
@@ -1,12 +1,13 @@
 import { describe, it } from "node:test"
 
+import { ValidationError } from "./server.ts"
 import { z, zv } from "./zod.ts"
 
 describe("zv", () => {
-	it("should throw if the value is invalid", (t) => {
+	it("should throw a ValidationError if the value is invalid", (t) => {
 		t.assert.throws(() => {
 			zv(123, z.string())
-		})
+		}, ValidationError)
 	})
 
 	it("should not throw if the value is valid", () => {
