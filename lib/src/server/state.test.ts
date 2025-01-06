6e64e405ffe78b9cabf4dff86a042762114a9426
Diogo Pais <dpais11@gmail.com>
2025-01-06 19:02:38 +0100

    Add tests

--- lib/src/server/state.test.ts
+++ lib/src/server/state.test.ts
@@ -43,4 +43,73 @@ describe("defineState", () => {
 			}, new Error("State has already been created."))
 		})
 	})
+
+	describe("replaceState", () => {
+		it("replaces the state", () => {
+			const { createState, replaceState } = defineState<string>()
+
+			const state = runWithStore(() => {
+				createState("hello")
+				return replaceState("world")
+			})
+
+			assert.strictEqual(state, "world")
+		})
+	})
+
+	describe("clearState", () => {
+		it("clears the state", () => {
+			const { createState, clearState } = defineState<string>()
+
+			const state = runWithStore(() => {
+				createState("hello")
+				clearState()
+				return createState("world")
+			})
+
+			assert.strictEqual(state, "world")
+		})
+	})
+
+	describe("useState", () => {
+		it("returns the state", () => {
+			const { createState, useState } = defineState<string>()
+
+			const state = runWithStore(() => {
+				createState("hello")
+				return useState()
+			})
+
+			assert.strictEqual(state, "hello")
+		})
+
+		it("returns undefined if the state has not been created", () => {
+			const { useState } = defineState<string>()
+
+			const state = runWithStore(() => useState())
+
+			assert.strictEqual(state, undefined)
+		})
+	})
+
+	describe("useStateOrThrow", () => {
+		it("returns the state", () => {
+			const { createState, useStateOrThrow } = defineState<string>()
+
+			const state = runWithStore(() => {
+				createState("hello")
+				return useStateOrThrow()
+			})
+
+			assert.strictEqual(state, "hello")
+		})
+
+		it("throws if the state has not been created", () => {
+			const { useStateOrThrow } = defineState<string>()
+
+			assert.throws(() => {
+				runWithStore(() => useStateOrThrow())
+			}, new Error("State has not been created."))
+		})
+	})
 })
