5fba0b17b520f3d49c3ac0fc1ee8b68cfd364874
Diogo Pais <dpais11@gmail.com>
2025-01-06 14:08:53 +0100

    Add tests

--- lib/src/result.test.ts
+++ lib/src/result.test.ts
@@ -1,22 +1,23 @@
+import assert from "node:assert"
 import { describe, it } from "node:test"
 
 import { err, errConst, ok, okConst } from "./result.ts"
 
 describe("ok", () => {
-	it("should return a result with the given value", (t) => {
-		t.assert.deepEqual(ok(42), { ok: true, value: 42 })
+	it("should return a result with the given value", () => {
+		assert.deepStrictEqual(ok(42), { ok: true, value: 42 })
 	})
 })
 
 describe("okConst", () => {
-	it("should return a result with the given value", (t) => {
-		t.assert.deepEqual(okConst(42), { ok: true, value: 42 })
+	it("should return a result with the given value", () => {
+		assert.deepStrictEqual(okConst(42), { ok: true, value: 42 })
 	})
 })
 
 describe("err", () => {
-	it("should return a result with the given error", (t) => {
-		t.assert.deepEqual(err("some_error"), {
+	it("should return a result with the given error", () => {
+		assert.deepStrictEqual(err("some_error"), {
 			ok: false,
 			error: "some_error",
 		})
@@ -24,8 +25,8 @@ describe("err", () => {
 })
 
 describe("errConst", () => {
-	it("should return a result with the given error", (t) => {
-		t.assert.deepEqual(errConst("some_error"), {
+	it("should return a result with the given error", () => {
+		assert.deepStrictEqual(errConst("some_error"), {
 			ok: false,
 			error: "some_error",
 		})
--- lib/src/zod.test.ts
+++ lib/src/zod.test.ts
@@ -1,16 +1,19 @@
+import assert from "node:assert"
 import { describe, it } from "node:test"
 
 import { ValidationError } from "./server.ts"
 import { z, zv } from "./zod.ts"
 
 describe("zv", () => {
-	it("should throw a ValidationError if the value is invalid", (t) => {
-		t.assert.throws(() => {
+	it("should throw a ValidationError if the value is invalid", () => {
+		assert.throws(() => {
 			zv(123, z.string())
 		}, ValidationError)
 	})
 
 	it("should not throw if the value is valid", () => {
-		zv("hello", z.string())
+		assert.doesNotThrow(() => {
+			zv("hello", z.string())
+		})
 	})
 })
