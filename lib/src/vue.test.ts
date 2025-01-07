64c679701bece069263243765a6295071688d21d
Diogo Pais <dpais11@gmail.com>
2025-01-07 19:24:11 +0100

    Add tests

--- lib/src/vue.test.ts
+++ lib/src/vue.test.ts
@@ -1,12 +1,13 @@
 import assert from "node:assert"
 import { beforeEach, describe, it, mock } from "node:test"
+import { setTimeout } from "node:timers/promises"
 
 const { computed, shallowRef } = await import("vue")
 
-const onBeforeUnmount = mock.fn()
+const onBeforeUnmount = mock.fn<(callback: () => void) => void>()
 
 beforeEach(() => {
-	onBeforeUnmount.mock.restore()
+	onBeforeUnmount.mock.resetCalls()
 })
 
 mock.module("vue", {
@@ -99,6 +100,35 @@ describe("useSubscription", () => {
 		assert.strictEqual(isSubscribing.value, true)
 
 		await assert.rejects(promise, new Error("Failed to subscribe."))
+
+		assert.strictEqual(isSubscribed.value, false)
+		assert.strictEqual(isSubscribing.value, false)
+	})
+
+	it("unsubscribes before the component is unmounted", async () => {
+		assert.strictEqual(onBeforeUnmount.mock.callCount(), 0)
+
+		const { isSubscribed, isSubscribing, subscribe } = useSubscription({
+			source: async () => Promise.resolve(new ReadableStream()),
+			onData() {
+				// do nothing
+			},
+		})
+
+		assert.strictEqual(onBeforeUnmount.mock.callCount(), 1)
+
+		await subscribe()
+
+		const callback = onBeforeUnmount.mock.calls[0]!.arguments[0]
+
+		await setTimeout(100)
+
+		assert.strictEqual(isSubscribed.value, true)
+		assert.strictEqual(isSubscribing.value, false)
+
+		callback()
+
+		await setTimeout(100)
 
 		assert.strictEqual(isSubscribed.value, false)
 		assert.strictEqual(isSubscribing.value, false)
